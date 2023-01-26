4bf8c7e514f6d96e5b542c3d2459d0642fe52553
stonasss <luis.b.santos96@gmail.com>
2023-01-26 16:02:00 -0300

    get de usuarios implementado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
+import infoRouter from "./routes/infoRoutes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use([authRouter])
+app.use([authRouter, infoRouter])
 
 const PORT = 5000;
 app.listen(PORT, () => {
