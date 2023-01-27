6a5239c683402791f18da129a82328086ef15ad6
stonasss <luis.b.santos96@gmail.com>
2023-01-27 20:20:01 -0300

    ajuste para deploy

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import infoRouter from "./routes/infoRoutes.js";
 
-const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use([authRouter, infoRouter])
 
-app.listen(PORT, () => {
-    console.log(`Servidor aberto na porta ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log("Servidor aberto");
 });
